2856af177a250c425df6d8f19a3897dd1e675e09
USATUKirill96 <kirill.ivanov96@gmail.com>
2020-02-24 12:46:35 +0500

    added commentaries to code

--- pkzdrav/rest_api/models.py
+++ pkzdrav/rest_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Directory(models.Model):
     """Модель справочника"""
-    id = models.AutoField()
+    id = models.AutoField() #идентификатор справочника
     name = models.CharField(max_length=250, verbose_name='Наименование')
     short_name = models.CharField(max_length=250, verbose_name='Короткое наименование')
     description = models.TextField(blank=True, verbose_name='Описание')
@@ -11,18 +11,21 @@ class Directory(models.Model):
     start_date = models.DateField(auto_now_add=True, verbose_name='Дата начала действия версии')
 
     class Meta:
+        """Преобразует вывод в панели администратора"""
         verbose_name_plural = 'Справочники'
         verbose_name = 'Справочник'
         ordering = ['short_name']
 
 
 class Directory_unit(models.Model):
+    """Модель элемента справочника"""
     directory = models.ForeignKey(Directory, on_delete=models.CASCADE, verbose_name = 'родительский идентификатор')
-    id = models.AutoField()
+    id = models.AutoField()  #идентификатор элемента
     code = models.CharField(blank=False, verbose_name='Код элемента')
     value = models.CharField(blank=False, verbose_name='Значение элемента')
 
     class Meta:
+        "Преобразует вывод в панели администратора"
         verbose_name_plural = 'Элементы справочника'
         verbose_name = 'Элемент справочника'
         ordering = ['code']
